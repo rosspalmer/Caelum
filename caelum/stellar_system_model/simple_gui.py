d6d2d086c07f22d493b200602d7f177583108cc1
Ross Palmer <palmer@opttek.com>
2017-01-06 16:08:02 -0700

    add float formating to gui

--- caelum/stellar_system_model/simple_gui.py
+++ caelum/stellar_system_model/simple_gui.py
@@ -2,8 +2,8 @@
 def display_system(system):
 
     print('========================')
-    print(' - Star Mass: %f' % system.star['mass'])
-    print(' - Star Lum: %f' % system.star['lum'])
+    print(' - Star Mass: %.2f' % system.star['mass'])
+    print(' - Star Lum: %.2f' % system.star['lum'])
 
     i = 1
 
@@ -13,20 +13,19 @@ def display_system(system):
         print('>> Planet %i' % i)
         print('')
         print(' - Type: %s' % planet.p['planet_type'])
-        print(' - Mass: %f' % planet.p['mass'])
-        print(' - Radius: %f' % planet.p['radius'])
-        print(' - Dist: %f AU' % planet.p['au'])
+        print(' - Mass: %.2f' % planet.p['mass'])
+        print(' - Dist: %.2f AU' % planet.p['au'])
         print('')
-        print(' - Period: %f Days' % planet.p['period'])
-        print(' - Rotation: %f Days' % planet.p['rotation'])
+        print(' - Period: %.2f Days' % planet.p['period'])
+        print(' - Rotation: %.2f Days' % planet.p['rotation'])
         print('')
-        print(' - Temp: %f K' % planet.p['temp'])
+        print(' - Temp: %.2f K' % planet.p['temp'])
         if 'atm' in planet.p:
-            print(' - Atmosphere: %f atm' % planet.p['atm'])
-        print(' - Alb: %f' % planet.p['alb'])
+            print(' - Atmosphere: %.2f atm' % planet.p['atm'])
+        print(' - Alb: %.2f' % planet.p['alb'])
         print('')
-        print(' - Radius: %f' % planet.p['radius'])
-        print(' - Density: %f' % planet.p['density'])
-        print(' - Gravity: %f g' % planet.p['g'])
+        print(' - Radius: %.2f' % planet.p['radius'])
+        print(' - Density: %.2f' % planet.p['density'])
+        print(' - Gravity: %.2f g' % planet.p['g'])
 
         i += 1
